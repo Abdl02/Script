8d4111b15dbd9a2d0708a9ce0139776dcaa554f4
mohammad.alzaro <mohammad.alzaro@digitinary.com>
2025-05-04 11:09:57 +0300

    yaml mapper util

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from util.config_loader import generate_token
 
-# Example usage
 if __name__ == "__main__":
     current_env_name = "localDev" 
     token = generate_token(current_env_name)
---
+++ runtime/template_processor.py
@@ -0,0 +1,1 @@
+from util.yaml_mapping import *
